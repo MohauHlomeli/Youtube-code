9d2f97242a3efaf664df95ace7387f9261e276aa
Mohau <39130450+MohauHlomeli@users.noreply.github.com>
2018-10-20 18:52:03 +0200

    Sprites code

--- BattleCity.java
+++ BattleCity.java
@@ -1,5 +1,6 @@
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -7,14 +8,12 @@ import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 import java.util.Random;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 
 // Created by Mohau
 // == 
 
-public class BattleCity extends Canvas implements Runnable, KeyListener {
+public class BattleCity extends Canvas implements Runnable,KeyListener {
 		
 	private static final long serialVersionUID = 1L;
 	public static final int WIDTH = 900; //SIZE of the width
@@ -24,26 +23,25 @@ public class BattleCity extends Canvas implements Runnable, KeyListener {
 	public Thread thread; // Main Thread
 	public Player player;
 	
-	int blocks[] = new int[32 * 32]; //1024 numbers/intergers
+	int blocks[] = new int[32 * 32]; // 1024 numbers/intergers
 	Random random = new Random();
-	int x;
-	int y;
+	public boolean[]keys= new boolean[150];
+	public boolean up = false;
+	public boolean down = false;
+	public boolean left = false;
+	public boolean right = false;
 	
-	public boolean keys[]= new boolean[150];
-	public boolean up,down,left,right;
+	int xpos = 1,ypos= 1;
+	int updates = 0;
 
 	
 
-	BufferedImage image = new BufferedImage(670,665,BufferedImage.TYPE_INT_RGB);
+	BufferedImage image = new BufferedImage(650,665,BufferedImage.TYPE_INT_RGB);
 	int pixels[] = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
-	
-	
 
 	
 	public BattleCity(){
-	
 		player = new Player();
-		
 		addKeyListener(this);
 		
 		for(int i=0;i<32 * 32;i++){ // 
@@ -78,17 +76,15 @@ public class BattleCity extends Canvas implements Runnable, KeyListener {
 	}
 	
 	public void run(){
-		setFocusable(true);
 		long lastTime = System.nanoTime(); // 
 		long timer = System.currentTimeMillis();
 		final double nanoseconds = 1000000000/60.0;  // im setting the desired FPS to 60 frames per second
 		int frames = 0;
-		int updates = 0;
 		double rate = 0;
 		
 		// All this variables are declared b4 we enter our main Game loop (While Loop)
 		
-
+		requestFocus();
 		while(isRunning){ // while our game is running, we will keep on updating and rendering to the screen
 			long now = System.nanoTime();
 			rate += (now - lastTime)/nanoseconds;  // rate += is the same as rate = rate + ....
@@ -102,7 +98,7 @@ public class BattleCity extends Canvas implements Runnable, KeyListener {
 				rate--;
 			}
 		
-			render(x,y);
+			render(xpos, ypos);
 			frames++;
 			
 			if(System.currentTimeMillis() - timer > 1000){
@@ -115,28 +111,30 @@ public class BattleCity extends Canvas implements Runnable, KeyListener {
 		stop();
 	}
 	
-	public void render(int xoff,int yoff){
+	
+	public void clearscreen(){
+		for(int i = 0;i<pixels.length;i++){
+			pixels[i]=0;
+		}
+	}
+	
+	public void render(int xpos, int ypos){
 		BufferStrategy bs = getBufferStrategy();
 		if(bs == null){
 			createBufferStrategy(3);
 			return;
 		}
-		
-		int xy = 1;
-		int yy = 1;
-		
-		
+		clearscreen();
 		for(int y=0;y<image.getHeight();y++){
-		
-			yy = y + yoff;
-			//if(yy<0 || yy>=image.getHeight())break;
-			for(int x=0;x<image.getWidth();x++){
-				xy = x + xoff;
-			pixels[x + y * image.getWidth()] = Sprite.dust.pixels[(x + y * image.getWidth())];
-			//pixels[x + y * image.getWidth()] = blocks[((xy /32) & 31) + ((yy/32) & 31) * 32];
-				//System.out.println(blocks[((xy /64) & 63) + ((yy/64) & 63) * 64]);
-			}
 			
+			int yy = y + ypos;
+			if(yy < 0 || yy>HEIGHT)continue;
+			
+			for(int x=0;x<image.getWidth();x++){
+				int xx = x + xpos;
+				if(xx < 0 || xx>WIDTH)continue;
+				pixels[x + y * image.getWidth()] = Sprite.bricks[((xx/4)&3) + ((yy/4) & 3) * 64];
+			}
 		
 		}
 		
@@ -145,43 +143,47 @@ public class BattleCity extends Canvas implements Runnable, KeyListener {
 		g.setColor(Color.black);
 		g.fillRect(0, 0, getWidth(), getHeight());
 		g.drawImage(image,2, 2, image.getWidth(), image.getHeight(),null);
+		g.setColor(new Color(238,249,38));
+		g.setFont(new Font("Goudy Stout",20,13));
+		g.drawString("Refresh rate: " + updates, 660, 30);
+		g.drawString("SCORE: 0",660,50);
+		g.drawString("LIVES: 0",660, 70);
 		//player.render(g);
 		g.dispose();
 		bs.show();
 	}
 	
 	public void update(){
-		if(up)y-=2;
-		if(right)x+=2;
-		if(down)y+=2;
-		if(left)x-=2;
+		if(up)ypos--;
+		if(right)xpos++;
+		if(down)ypos++;
+		if(left)xpos--;
 	
 		player.x = 1;
 		player.y = 1; 
-		
 	//System.out.println("Update..");	
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		if(e.getKeyCode()== KeyEvent.VK_W || e.getKeyCode() == KeyEvent.VK_UP) up = true;
-		if(e.getKeyCode()== KeyEvent.VK_D || e.getKeyCode() == KeyEvent.VK_RIGHT) right = true;
-		if(e.getKeyCode()== KeyEvent.VK_S || e.getKeyCode() == KeyEvent.VK_DOWN) down = true;
-		if(e.getKeyCode()== KeyEvent.VK_A || e.getKeyCode() == KeyEvent.VK_LEFT) left = true;
+		if(e.getKeyCode()== KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D)right = true;
+		if(e.getKeyCode()== KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S)down = true;
+		if(e.getKeyCode()== KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A)left = true;
+		if(e.getKeyCode()== KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W)up = true;
 		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		if(e.getKeyCode()== KeyEvent.VK_W || e.getKeyCode() == KeyEvent.VK_UP) up = false;
-		if(e.getKeyCode()== KeyEvent.VK_D || e.getKeyCode() == KeyEvent.VK_RIGHT) right = false;
-		if(e.getKeyCode()== KeyEvent.VK_S || e.getKeyCode() == KeyEvent.VK_DOWN) down = false;
-		if(e.getKeyCode()== KeyEvent.VK_A || e.getKeyCode() == KeyEvent.VK_LEFT) left = false;
+		if(e.getKeyCode()== KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D)right = false;
+		if(e.getKeyCode()== KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S)down = false;
+		if(e.getKeyCode()== KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A)left = false;
+		if(e.getKeyCode()== KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W)up = false;
 		
 	}
 
 	@Override
-	public void keyTyped(KeyEvent e) {
+	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
